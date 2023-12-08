caee135c1f9e85c09b9cf9f10e5fdbf5621e27e1
Adam Drew <addrew@redhat.com>
2023-12-08 08:51:34 -0500

    Add build script

---
+++ build_deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Get the first 7 characters of the current git commit SHA
+GIT_SHA=$(git rev-parse --short=7 HEAD)
+
+if [ -z "$GIT_SHA" ]; then
+    echo "Error: Git commit hash not found."
+    exit 1
+fi
+
+# Define the image name
+IMAGE_NAME="firelink-backend:$GIT_SHA"
+
+# Build the Docker image
+echo "Building Docker image: $IMAGE_NAME"
+docker build -t "$IMAGE_NAME" .
+
+# Tag the image for Quay
+QUAY_IMAGE="quay.io/cloudservices/$IMAGE_NAME"
+echo "Tagging image for Quay: $QUAY_IMAGE"
+docker tag "$IMAGE_NAME" "$QUAY_IMAGE"
+
+# Push the image to Quay
+echo "Pushing image to Quay: $QUAY_IMAGE"
+docker push "$QUAY_IMAGE"
+
+echo "Image successfully pushed to Quay: $QUAY_IMAGE"
