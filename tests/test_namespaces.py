9170449d02409311e426b5d97757bb5f837efc89
Adam Drew <addrew@redhat.com>
2024-02-16 18:24:14 -0500

    test refactor

--- tests/test_namespaces.py
+++ tests/test_namespaces.py
@@ -4,20 +4,24 @@ from firelink.Namespace import Namespace
 import json
 import time 
 
+def _reserve_namespace():
+    ns_controller = Namespace()
+    response = json.loads(ns_controller.reserve({"force": False, "pool": "default", "duration": "1h", "requester": "firelink-backend-tests"}))    
+    return response
+
 def test_namespace_list():
     namespaces = Namespace().list()
     assert len(namespaces) > 0
 
 def test_namespace_reserve():
     ns_controller = Namespace()
-    response = json.loads(ns_controller.reserve({"force": False, "pool": "default", "duration": "1h", "requester": "firelink-backend-tests"}))    
+    response = _reserve_namespace()    
     assert response["completed"] == True
     ns_controller.release({"namespace": response["namespace"]})
 
 def test_namespace_release():
     ns_controller = Namespace()
-    namespace = json.loads(ns_controller.reserve({"force": False, "pool": "default", "duration": "1h", "requester": "firelink-backend-tests"}))  
-    time.sleep(5)
+    namespace = _reserve_namespace() 
     response = json.loads(ns_controller.release({"namespace": namespace["namespace"]}))
     assert response["completed"] == True
 
@@ -28,7 +32,7 @@ def test_namespace_release_no_name_specified():
 
 def test_namespace_describe():
     ns_controller = Namespace()
-    namespace = json.loads(ns_controller.reserve({"force": False, "pool": "default", "duration": "1h", "requester": "firelink-backend-tests"}))  
+    namespace = _reserve_namespace() 
     response = json.loads(ns_controller.describe(namespace["namespace"]))
     assert response["completed"] == True
     ns_controller.release({"namespace": namespace["namespace"]})
